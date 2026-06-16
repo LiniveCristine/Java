cc123762ace15c9f6252647ca560db895fcf7dd9
Linive Cristine <linive.dev@gmailmail.com>
2026-06-16 14:08:06 -0300

    Refatoração de diretórios

---
+++ 02-logica-basica/exercicios/src/Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+    static void main(String[] args) {
+        int b;
+        double a;
+
+        a = 5.3;
+        b = (int) a;
+
+        System.out.println(b);
+    }
+}
+
